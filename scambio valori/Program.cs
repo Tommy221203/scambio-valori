19e42428699599c615c7cb5f8532064fb68b4602
Tommy221203 <55913204+Tommy221203@users.noreply.github.com>
2019-10-27 18:34:11 +0100

    Add project files.

---
+++ scambio valori/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace scambio_valori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci il primo numero:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserisci il secondo numero:");
+            int b = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
